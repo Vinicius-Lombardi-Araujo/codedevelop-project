edf69928840ee10fd524c982b5dc5fa00b11fa13
vinicius <vini_laraujo@hotmail.com>
2024-06-12 18:38:34 -0300

    First commit

---
+++ codedevelop-project-api/src/main/java/com/vinicius/codedevelop_project_api/CodedevelopProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.codedevelop_project_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodedevelopProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodedevelopProjectApiApplication.class, args);
+	}
+
+}
---
+++ codedevelop-project-api/src/test/java/com/vinicius/codedevelop_project_api/CodedevelopProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.codedevelop_project_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodedevelopProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
