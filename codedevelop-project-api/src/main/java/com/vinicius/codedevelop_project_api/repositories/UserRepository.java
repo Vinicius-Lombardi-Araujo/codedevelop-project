6e3f539b88ae893ffc6ae42eff0e902f5957ddb4
vinicius <vini_laraujo@hotmail.com>
2024-06-12 18:52:05 -0300

    Crete UserRepository

---
+++ codedevelop-project-api/src/main/java/com/vinicius/codedevelop_project_api/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.vinicius.codedevelop_project_api.repositories;
+
+import com.vinicius.codedevelop_project_api.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByEmail(String email);
+}
